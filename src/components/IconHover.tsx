d27fd96ecf3ce9cae052d162002423ecb3e918a3
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2024-01-04 17:30:30 +0900

    minor css edits

--- src/components/IconHover.tsx
+++ src/components/IconHover.tsx
@@ -10,7 +10,7 @@ export function IconHoverEffect({ children, red = false, additionalClasses = "",
 
   const colorClasses = red
     ? "outline-red-400 hover:bg-red-200 group-hover-bg-red-200 group-focus-visible:bg-red-200 focus-visible:bg-red-200"
-    : "outline-gray-400 hover:bg-gray-200 group-hover-bg-gray-200 group-focus-visible:bg-gray-200 focus-visible:bg-gray-200";
+    : "outline-gray-600 hover:bg-gray-400 group-hover-bg-gray-400 group-focus-visible:bg-gray-400 focus-visible:bg-gray-400";
 
   return (
     <div className={`rounded-full px-6 py-2 transition-colors duration-200 flex items-center gap-2 ${colorClasses} ${additionalClasses}`}>
